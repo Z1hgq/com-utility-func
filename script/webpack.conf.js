efd2f2072711e738c7f85ccb538b231dacbe8964
gqzhang <gqzhang@streamax.com>
2020-01-19 22:13:22 +0800

    change config

--- script/webpack.conf.js
+++ script/webpack.conf.js
@@ -1,17 +1,15 @@
 const webpack = require("webpack");
 const path = require("path");
 
-const pkg = require("../package.json");
-
 const rootPath = path.resolve(__dirname, "../");
 
 const config = {
     mode: "production",
     entry: path.resolve(rootPath, "src", "index.js"),
     output: {
-        filename: `${pkg.name}.min.js`,
+        filename: `smutils.min.js`,
         path: path.resolve(rootPath, "min"),
-        library: `${pkg.name}`,
+        library: `smutils`,
         libraryTarget: "umd"
     },
     module: {
